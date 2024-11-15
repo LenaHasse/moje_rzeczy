f3a7253c1faa574c32b022a44e517c352baf3f07
LenaHasse <149078095+LenaHasse@users.noreply.github.com>
2024-11-15 09:55:33 +0100

    Add files via upload

--- wisielec.py
+++ wisielec.py
@@ -1,3 +1,5 @@
+import random
+
 with open('poziom_latwy.txt', 'r') as plik1:
     hasla_latwe=plik1.read()
 
@@ -7,13 +9,34 @@ with open('poziom_trudny.txt', 'r') as plik3:
 with open('poziom_sredni.txt', 'r') as plik2:
     hasla_srednie=plik2.read()
 
-
-hasla_latwe.lower()
-hasla_srednie.lower()
-hasla_trudne.lower()
+zgadniecie=True
 
 lista_latwa= hasla_latwe.split()
 lista_srednia=hasla_srednie.split()
 lista_trudna=hasla_trudne.split()
 
-print(lista_srednia)
+
+haslo=str(lista_latwa[random.randint(0,len(lista_latwa))]).lower()
+dlugosc_hasla=len(haslo) #ilosc liter w hasle
+wynik=0             #zbieranie punktow za kazda litere, gdy wynik==dlugosci hasla to haslo zostalo zgadniete
+
+zakryte_haslo=[]
+for i in haslo:
+    zakryte_haslo.append('_')
+
+print(' '.join(zakryte_haslo))
+print(haslo) #USUN POTEM
+print('\n')
+
+while dlugosc_hasla!=wynik:
+    litera = str(input('Podaj litere: '))
+    if litera in haslo:
+        for i in range(len(haslo)):
+            if haslo[i]==litera:
+                zakryte_haslo[i]=litera
+        wynik+=1
+    else:
+        print('Źle!')
+    print(' '.join(zakryte_haslo))
+
+print('Brawo, zgadles')
