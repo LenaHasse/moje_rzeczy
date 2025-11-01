dd5a573babdc513c5989339056d72bb9cd4ed7ef
Lena Hasse <lena.hasse2@gmail.com>
2025-11-01 17:56:26 +0100

    zadanka na indeks

---
+++ indeks/matura_podstawowa_zad5.py
@@ -0,0 +1,44 @@
+#zad1
+with open('liczby.txt','r') as plik:
+    lista=[int(x) for x in plik]
+
+    najwieksza=0
+    for i in lista:
+        if i%2==0:
+            if i>najwieksza:
+                najwieksza=i
+
+    wyniki=open('wyniki5.txt','w')
+    wyniki.write('5.1\n')
+    wyniki.write(str(najwieksza))
+    wyniki.write('\n')
+    #zad2
+    wyniki.write('5.2\n')
+    palindromy=[]
+    for i in lista:
+        if str(i)==str(i)[::-1]:
+            wyniki.write(str(i))
+            wyniki.write('\n')
+
+    #zad3
+    wyniki.write('5.3\n')
+    def suma_cyfr(n):
+        sum=0
+        while n>0:
+            sum+=n%10
+            n//=10
+        return sum
+
+    for i in lista:
+        if suma_cyfr(i)>30:
+            wyniki.write(str(i))
+            wyniki.write('\n')
+
+    suma_wszystkich=0
+    for i in lista:
+        suma_wszystkich+=suma_cyfr(i)
+
+    wyniki.write(f'Suma wszystkich: {suma_wszystkich}')
+    wyniki.close()
+
+
