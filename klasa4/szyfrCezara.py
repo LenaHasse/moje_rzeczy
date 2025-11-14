8a7ea66e2f1413a9a69406289d10ca644f8ae507
Lena Hasse <149078095+LenaHasse@users.noreply.github.com>
2025-11-14 10:43:23 +0100

    Refactor szyfrujCezara and add deszyfruj_cezara
    Refactor Caesar cipher function to handle both upper and lower case letters. Added decryption function.

--- klasa4/szyfrCezara.py
+++ klasa4/szyfrCezara.py
@@ -1,20 +1,21 @@
 def szyfrujCezara(tekst,przesuniecie):
     wynik=''
     for znak in tekst:
-        if znak.isupper():
-            znak=ord(znak)
-            znak+=5
-            znak+=znak%26
-            znak=chr(znak)
+        if znak.isalpha():
+            if znak.isupper():
+                pozycja=ord(znak)-ord('A')
+                nowa_pozycja=(pozycja + przesuniecie)%26
+                nowy_znak=chr(nowa_pozycja+ord('A'))
+                wynik+=nowy_znak
+            else:
+                pozycja = ord(znak) - ord('a')
+                nowa_pozycja = (pozycja + przesuniecie) % 26
+                nowy_znak = chr(nowa_pozycja + ord('a'))
+                wynik += nowy_znak
+        else:
             wynik+=znak
-        if znak.islower():
-            znak = ord(znak)
-            znak += 5
-            znak+=znak%26
-            znak = chr(znak)
-            wynik += znak
-
     return wynik
 
-
+def deszyfruj_cezara(tekst,przesuniecie):
+    return szyfrujCezara(tekst,-przesuniecie)
 print(szyfrujCezara('AbcSDef',3))
