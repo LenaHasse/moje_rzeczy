13cf33a963f2f2b2e4292b9d7cb1efb7bbf36236
Lena <lena.hasse2@gmail.com>
2025-12-18 09:12:50 +0100

    symbole CKE

---
+++ klasa4/symbole.py
@@ -0,0 +1,24 @@
+with open('symbole.txt','r')as plik:
+    lista=[x.strip() for x in plik]
+    #2.1
+    zad1=[i for i in lista if i==i[::-1]]
+    htf = "\n".join(zad1)
+    #print(zad1)
+    #print(htf)
+
+    #zad2.2
+    sprawdzany=lista[0][0]
+    sprawdzajka=0
+    zad2=0
+    zad2_lista=[]
+    for i in range(len(lista)-2):
+        for j in range(len(lista[i])-2):
+            wiersz1=
+    print(zad2)
+    print(zad2_lista)
+
+
+
+
+
+
